30cb666013efee393b408cb6ec1cb52d90afd95c
jastrzembowski <jastrzembowskikamil@gmail.com>
2022-07-25 12:14:56 +0200

    favicon, prep for push to server

--- src/components/CompSlides.jsx
+++ src/components/CompSlides.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import Slider from "react-slick";
 
-import logo0 from "../images/comp/logo0.png";
-import logo1 from "../images/comp/logo1.png";
-import logo2 from "../images/comp/logo2.png";
-import logo3 from "../images/comp/logo3.png";
-import logo4 from "../images/comp/logo4.jpg";
+
+import logo4 from "../images/comp/logo4.png";
 import logo5 from "../images/comp/logo5.png";
 import logo6 from "../images/comp/logo6.png";
 import logo7 from "../images/comp/logo7.png";
 import logo8 from "../images/comp/logo8.png";
-import logo9 from "../images/comp/logo9.jpg";
+import logo9 from "../images/comp/logo9.png";
 
 function CompSlides() {
   const settings = {
-    slidesToShow: 9,
+    slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
     dots: false,
@@ -39,10 +36,12 @@ function CompSlides() {
     <div className="comp-slides-container">
       <h1>We are trusted by:</h1>
       <Slider {...settings}>
-        <img src={logo0} alt="comp-logo" className="comp-logo"></img>
-        <img src={logo1} alt="comp-logo" className="comp-logo"></img>
-        <img src={logo2} alt="comp-logo" className="comp-logo"></img>
-        <img src={logo3} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo4} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo5} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo6} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo7} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo8} alt="comp-logo" className="comp-logo"></img>
+        <img src={logo9} alt="comp-logo" className="comp-logo"></img>
         <img src={logo4} alt="comp-logo" className="comp-logo"></img>
         <img src={logo5} alt="comp-logo" className="comp-logo"></img>
         <img src={logo6} alt="comp-logo" className="comp-logo"></img>
